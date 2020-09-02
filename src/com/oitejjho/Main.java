4439b1c29471b665a95f5d17deba74a68b060bae
Oitejjho Dutta <oitejjho.dut@ascendcorp.com>
2020-09-02 15:17:20 +0700

    more tests are added.

--- src/com/oitejjho/Main.java
+++ src/com/oitejjho/Main.java
@@ -1,12 +1,13 @@
 package com.oitejjho;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         //        System.out.println("hello");
 //
@@ -17,7 +18,10 @@ public class Main {
 
         String dateFormatter = "yyyy[-][/][:]MM[-][/][:]dd[][[ ]['T']HH[-][/][:]mm[-][/][:][ss][.SSS][XXX][Z]]";
 
+        String dateFormatter1 = "[yyyy][dd][MM][-][/][:][yyyy][MM][dd][-][/][:][yyyy][MM][dd][][[ ]['T'][HH][-][/][:][mm][-][/][:][ss][.SSS][XXX][Z]]";
+
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormatter);
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern(dateFormatter1);
 
         /*LocalDate localDate = LocalDate.parse("2014-02-18", dateTimeFormatter);
         localDate = LocalDate.parse("2014/02/18", dateTimeFormatter);
@@ -40,14 +44,54 @@ public class Main {
         localDateTime = LocalDateTime.parse("2014:02:18T12:12:12.777", dateTimeFormatter);*/
 
 
-
 //        LocalDateTime localDateTime = LocalDateTime.parse("2014-02-18T12-12-12.777+0700", dateTimeFormatter);
 //        localDateTime = LocalDateTime.now();
 //        localDateTime = LocalDateTime.parse("2014/02/18T12/12/12.777+0700", dateTimeFormatter);
 //        localDateTime = LocalDateTime.parse("2014:02:18T12:12:12.777+0700", dateTimeFormatter);
 
 //        LocalDateTime localDateTime = LocalDateTime.parse("25/03/2020 11:37:38", dateTimeFormatter);
-        LocalDateTime localDateTime1 = LocalDateTime.parse("2018-03-29T14:46:36.000+07:00", dateTimeFormatter);
+        LocalDateTime localDateTime1 = LocalDateTime.parse("2018-03-29T14:46:36.000+07:00", dateTimeFormatter1);
+        String formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+        localDateTime1 = LocalDateTime.parse("2018-03-29T14:46:36+07:00", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+        localDateTime1 = LocalDateTime.parse("2018-03-29T14:46:36", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+        localDateTime1 = LocalDateTime.parse("2018-03-29 14:46:36", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+        localDateTime1 = LocalDateTime.parse("2018-03-29 14:46", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+        localDateTime1 = LocalDateTime.parse("2018-03-29 14", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+        LocalDate localDate = LocalDate.parse("2018-03-29", dateTimeFormatter1);
+        formattedDateTime = localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        System.out.println(formattedDateTime);
+
+
+
+
+
+
+        //25/03/2020 11:37:38
+        localDateTime1 = LocalDateTime.parse("25/03/2020 11:37:38", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+        localDateTime1 = LocalDateTime.parse("2020/03/25 11:37:38", dateTimeFormatter1);
+        formattedDateTime = localDateTime1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(formattedDateTime);
+
+//        DateTimeUtil.formatDateTime(externalBankAccount.getTerminateDate(), DateFormatConstants.DEFAULT_YYYY_MM_DD_FORMAT, DateFormatConstants.DATE_FORMAT_DD_MMM_YYYY_WITH_SPACE);
 //        localDateTime = LocalDateTime.parse("2014/02/18T12/12/12.777+07:00", dateTimeFormatter);
 //        localDateTime = LocalDateTime.parse("2014:02:18T12:12:12.777+07:00", dateTimeFormatter);
 
